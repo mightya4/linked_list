f62f7be46865cef51c1ea3f85aaec2054f25f337
Akeem <akeem.mighty@gmail.com>
2022-12-20 22:40:47 -0500

    Create Linked List Class with Single Head Node

--- linked_list.py
+++ linked_list.py
@@ -3,3 +3,8 @@ class Node:
     def __init__(self, data = None, next = None):
         self.data = data
         self.next = next
+
+#Linked List with a single head node
+class LinkedList:
+    def __init__(self):
+        self.head = None
